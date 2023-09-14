faaea11378c3f7ad18f54bc0c9b55115e4826984
pashutk <me@pashutk.ru>
2023-09-15 01:00:17 +0200

    Index page wip

---
+++ src/components/HomepageFeatures.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+import clsx from "clsx";
+import styles from "./HomepageFeatures.module.css";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import Link from "@docusaurus/Link";
+
+type FeatureItem = {
+  title: string;
+  image?: string;
+  description: JSX.Element;
+  link: string;
+  linkText: string;
+};
+
+const FeatureList: FeatureItem[] = [
+  {
+    title: "Craft Your Comfort",
+    description: (
+      <>
+        Comprehensive guides to help you assemble your Ergonaut Keyboard and
+        customize it to your needs.
+      </>
+    ),
+    // image: "/img/undraw-mg-community.svg",
+    link: "/guides",
+    linkText: "Build Guides",
+  },
+  {
+    title: "Ergo Insights",
+    // image: "/img/undraw-mg-book.svg",
+    description: (
+      <>
+        Stay updated with the latest trends in ergonomic technology, product
+        updates, and user stories from the Ergonaut Keyboards community.
+      </>
+    ),
+    link: "/blog",
+    linkText: "Our Blog",
+  },
+  {
+    title: "In-depth Knowledge, Simplified",
+    // image: "/img/undraw-mg-mundo.svg",
+    description: (
+      <>
+        Find all technical specifications, troubleshooting steps, and detailed
+        instructions to enhance your Ergonaut Keyboard experience.
+      </>
+    ),
+    link: "/tech-specs",
+    linkText: "Tech Specs",
+  },
+];
+
+function Feature({ title, image, description, link, linkText }: FeatureItem) {
+  return (
+    <div className={clsx("col col--4", "padding-bottom--lg")}>
+      {image && (
+        <div className="text--center">
+          <img
+            className={styles.featureSvg}
+            src={useBaseUrl(image)}
+            alt={title}
+          />
+        </div>
+      )}
+      <div className="text--center padding-horiz--md">
+        <h3 className="header-pill">{title}</h3>
+        <p>{description}</p>
+        <Link className="button button--secondary button--lg" to={link}>
+          {linkText}
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures(): JSX.Element {
+  return (
+    <>
+      <section className={clsx(styles.features)}>
+        <div className={clsx(styles.featuresContainer, "margin--xl")}>
+          <div className={clsx("row")}>
+            {FeatureList.map((props, idx) => (
+              <Feature key={idx} {...props} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
-import styles from './index.module.css';
+import styles from "./index.module.css";
+import WaveTopShapeDivider from "../components/WaveTopShapeDivider";
+import WaveBottomShapeDivider from "../components/WaveBottomShapeDivider";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero", styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+          <Link className="button button--primary button--lg" to="/docs/intro">
+            Getting Started
           </Link>
         </div>
       </div>
@@ -27,14 +27,17 @@ function HomepageHeader() {
 }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Description will go into a meta tag in <head />"
+    >
       <HomepageHeader />
       <main>
+        <WaveTopShapeDivider />
         <HomepageFeatures />
+        <WaveBottomShapeDivider />
       </main>
     </Layout>
   );
