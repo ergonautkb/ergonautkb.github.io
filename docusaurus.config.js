3944d267462f2be540e5702b9a9a439de7c0ea6e
Rafael Yumagulov <yumagulovrn@gmail.com>
2023-10-13 11:58:45 +0300

    fix navbar (#4)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -72,9 +72,9 @@ const config = {
         items: [
           {
             type: "docSidebar",
-            sidebarId: "tutorialSidebar",
+            sidebarId: "docsSidebar",
             position: "left",
-            label: "Tutorial",
+            label: "Docs",
           },
           { to: "/blog", label: "Blog", position: "left" },
           {
