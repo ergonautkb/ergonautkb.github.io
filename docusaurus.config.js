02d1233621c1d08723a81daa39d7daf04ea2bcf1
pashutk <me@pashutk.ru>
2023-11-06 01:05:42 +0100

    Change the website url, configure sitemap plugin

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://ergonautkb.github.io",
+  url: "https://ergonautkb.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
@@ -49,6 +49,17 @@ const config = {
           customCss: require.resolve("./src/css/custom.css"),
         },
       }),
+    ],
+    [
+      "@docusaurus/preset-classic",
+      {
+        sitemap: {
+          changefreq: "weekly",
+          priority: 0.5,
+          ignorePatterns: ["/tags/**"],
+          filename: "sitemap.xml",
+        },
+      },
     ],
   ],
 
